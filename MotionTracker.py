d139c46877d97e9d3d878a23716671aa62a4d0c6
Coleman Lab at UF <colemanlabuf@gmail.com>
2017-11-03 11:51:47 -0400

    Undo merge mistake

--- MotionTracker.py
+++ MotionTracker.py
@@ -40,38 +40,6 @@ length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print length
 
 while True:
-<<<<<<< HEAD
-    ret,frame = cap.read()
-    if not ret:
-        break
-
-    key = cv2.waitKey(25) & 0xFF
-    frameNumber+=1
-
-    cv2.imshow(frame_title,frame)
-
-    if key == ord("p"):
-        while cap.isOpened():
-            key = cv2.waitKey(0) & 0xFF
-
-            if key == ord('p'):
-                break
-            elif key == ord('m'):
-                cv2.setMouseCallback(frame_title, selectCorners)
-            elif key == ord('s'):
-                saved = {"corners":corners}
-                pickle.dump(saved,open(file_path[:-5] + ".p","wb"))
-                print('Done')
-                cv2.destroyAllWindows()
-                cap.release()
-            elif key == ord('q') or key == 27:
-                cv2.destroyAllWindows()
-                cap.release()
-
-    elif key == ord('q') or key == 27:
-        cv2.destroyAllWindows()
-        cap.release()
-=======
 	ret,frame = cap.read()
 
 	if not ret:
@@ -104,7 +72,6 @@ while True:
 	elif key == ord('q') or key == 27:
 		cv2.destroyAllWindows()
 		cap.release()
->>>>>>> origin/master
 
 
 cap = cv2.VideoCapture(file_path)
@@ -118,96 +85,6 @@ print len(corners)
 roi = frame[corners[0][1]:corners[1][1], corners[0][0]:corners[1][0]]
 roi2 = frame[corners[2][1]:corners[3][1], corners[2][0]:corners[3][0]]
 
-<<<<<<< HEAD
-
-while True:
-
-        ret,frame = cap.read()
-        text = "Unoccupied"
-        
-    
-        if not ret:
-            break;
-    
-        key = cv2.waitKey(25) & 0xFF
-        frameNumber+=1
-        cv2.rectangle(frame, corners[0], corners[1], (0, 255, 0), 2)
-    
-        roi = frame[corners[0][1]:corners[1][1], corners[0][0]:corners[1][0]]
-    
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-    
-        if firstFrame is None:
-            firstFrame = gray
-            continue
-    
-        frameDelta = cv2.absdiff(firstFrame, gray)
-        thresh = cv2.threshold(frameDelta, 10, 255, cv2.THRESH_BINARY)[1]
-    
-        thresh = cv2.dilate(thresh, None, iterations=1)
-        (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    
-        for c in cnts:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(c) < 10:
-                continue
-    
-            # compute the bounding box for the contour, draw it on the frame,
-            # and update the text
-            text = "Occupied"
-            
-        # draw the text and timestamp on the frame
-        cv2.putText(frame, "Chamber Status: {}".format(text), (10, 20),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            
-        if text == 'Occupied':
-            frametracker.append( 1 )
-        elif text == 'Unoccupied':
-            frametracker.append( 0 )
-        
-        cv2.imshow("Thresh", thresh)
-        cv2.imshow("Frame Delta", frameDelta)
-    
-        cv2.imshow(frame_title,frame)
-        #cv2.imshow('Crop', roi)
-        
-        if key == ord('q') or key == 27:
-            cv2.destroyAllWindows()
-            cap.release()
-
-# Save data
-with open(file_path+'mid.pickle', 'w') as f:  # Python 3: open(..., 'wb')
-    pickle.dump({'frametracker': frametracker,
-                 'corners':corners,
-                 'frameNumber':frameNumber,
-                 'file_path': file_path,
-                 'frame_title': frame_title,
-                 }, f)
-
-## Load data
-#with open(file_pathR+'right.pickle') as f:  # Python 3: open(..., 'rb')
-#    alldataR = pickle.load(f)
-#    aR = alldataR['frametracker']
-#    #pad with nans/zeros to align frames (frameNumber - len(a))
-#    
-#with open(file_pathM+'mid.pickle') as f:  # Python 3: open(..., 'rb')
-#    alldataM = pickle.load(f)
-#    aM = alldataM['frametracker']
-#    #pad with nans/zeros to align frames (frameNumber - len(a))
-#    
-## Check data
-#from matplotlib import pyplot as plt
-#import numpy as np
-#
-#aRshift = np.array(aR)+1
-#
-#plt.plot(aRshift, 'b')
-#plt.plot(aM, 'r')
-#plt.ylim([-2,4])
-#plt.title('tracked in right and middle chambers')
-#plt.legend({'R', 'M'})
-=======
 gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -294,7 +171,6 @@ for a in range(length):
            text3 = 'Unoccupied'
            frametracker3.append( 0 )           
     
->>>>>>> origin/master
 
 	bar.next()
 
